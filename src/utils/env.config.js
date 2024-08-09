5aab433c4c9db829daed7607bc038925290c1c4e
Jam <42326027+benjammin4dayz@users.noreply.github.com>
2024-08-09 08:08:28 -0400

    Refactor env resolution to handle type conversion

--- src/utils/env.config.js
+++ src/utils/env.config.js
@@ -10,24 +10,40 @@ export const PORT = Number.parseInt(__env__('PORT', false, 3000), 10);
  * App environment; currently only affects the logger module
  * @default false
  */
-export const DEV = Boolean(
-  __env__('DEV', false, false).toLowerCase() === 'true'
-);
+export const DEV = __env__('DEV', false, false);
 
 /**
  *
  * @param {string} name
- * @param {?boolean} [required]
- * @param {?string} [defaultValue]
- * @returns {string}
+ * @param {boolean} required
+ * @param {*} fallback
+ * @returns
  */
-function __env__(name, required, defaultValue) {
-  const value = process.env[name];
-  if (required && !value) {
+function __env__(name, required, fallback) {
+  const parse = value => {
+    if (value) value = String(value).toLowerCase().trim();
+    switch (value) {
+      case 'true':
+        return true;
+      case 'false':
+        return false;
+      case 'null':
+        return null;
+      case 'undefined':
+        return undefined;
+      default:
+        return value;
+    }
+  };
+
+  const value = parse(process.env[name]);
+
+  if (!required && fallback === undefined) {
+    throw new Error(`Missing fallback for optional .env key: ${name}`);
+  }
+  if (required && value === undefined && fallback === undefined) {
     throw new Error(`Missing required .env key: '${name}'`);
   }
-  if (!value) {
-    return defaultValue || '';
-  }
-  return String(value);
+
+  return value ?? fallback;
 }
